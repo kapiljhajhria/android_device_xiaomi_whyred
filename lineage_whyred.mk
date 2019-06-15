ad134cd3288159a9bb28ba839d9af7e432340031
kapil jhajhria <jhajhria44@gmail.com>
2019-06-15 05:17:17 +0000

    add miui camera

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -19,6 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
